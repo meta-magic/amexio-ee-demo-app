73418ad298d8c9e404fb4556a5650447eb1e01a7
kedar8 <kedar.kokil@metamagic.in>
2019-11-15 13:24:59 +0530

    migration:Angular version from 6 to 7.

--- src/app/windows/window.component.ts
+++ src/app/windows/window.component.ts
@@ -2,34 +2,34 @@
  * Created by Ashwini on 07/03/19.
  */
 
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'window-ce-demo', templateUrl: './window.component.html'
 })
 export class CreativeWindowDemo {
-   showSideModal: boolean;
-   showCenterModal:boolean;
-   showLoginWindow:boolean;
-   showInfoModal:boolean;
-   showFrameWindow:boolean;
+  showSideModal: boolean;
+  showCenterModal: boolean;
+  showLoginWindow: boolean;
+  showInfoModal: boolean;
+  showFrameWindow: boolean;
   constructor() {
-     
+
+  }
+  onSideModalClick() {
+    this.showSideModal = !this.showSideModal;
+  }
+  showInfoModalClick() {
+    this.showInfoModal = !this.showInfoModal;
+  }
+  onloginWindowClick() {
+    this.showLoginWindow = !this.showLoginWindow;
+  }
+  onCenterModalClick() {
+    this.showCenterModal = !this.showCenterModal;
+  }
+  onFrameModalClick() {
+    this.showFrameWindow = !this.showFrameWindow;
+  }
 }
-  onSideModalClick(){
-   this.showSideModal = !this.showSideModal;
-  }
-  showInfoModalClick(){
-   this.showInfoModal = !this.showInfoModal;
-  } 
-  onloginWindowClick(){
-   this.showLoginWindow = ! this.showLoginWindow;
-  }
-  onCenterModalClick(){
-    this.showCenterModal = ! this.showCenterModal;
-  }
-  onFrameModalClick(){
-   this.showFrameWindow =! this.showFrameWindow;
-  }
-} 
 
